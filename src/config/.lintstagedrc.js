a958505975f04de94f381cde9e418431858f813f
Dustin Ruetz <dustin.ruetz@telus.com>
2020-02-02 19:34:24 -0500

    build(deps): update packages to latest major versions

--- src/config/.lintstagedrc.js
+++ src/config/.lintstagedrc.js
@@ -5,11 +5,11 @@ const drScripts = resolveDrScripts()
 
 module.exports = {
   // `doctoc` package automatically maintains readme.md table of contents
-  'readme.md': [`${doctoc} --notitle`, 'git add'],
+  'readme.md': [`${doctoc} --notitle`],
   '*.+(css|graphql|html|js|json|jsx|less|md|mdx|scss|ts|tsx|vue|yaml|yml)': [
     isOptedOut('autoformat', null, `${drScripts} format`),
     `${drScripts} lint`,
     `${drScripts} test --findRelatedTests`,
-    isOptedOut('autoformat', null, 'git add'),
+    isOptedOut('autoformat', null),
   ].filter(Boolean),
 }
