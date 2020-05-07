f06b1756a9d40d083d31d33a98d9753190384574
Dustin Ruetz <dustin.ruetz@telus.com>
2020-05-07 17:03:02 -0400

    fix(lint): require eslint-plugin-react-hooks recommended config rules

--- src/config/eslint-plugins/react.js
+++ src/config/eslint-plugins/react.js
@@ -1,9 +1,15 @@
-// require the recommended rules from eslint-plugin-react/index.js
-// https://github.com/yannickcr/eslint-plugin-react/blob/master/index.js
-const {rules} = require('eslint-plugin-react').configs.recommended
+/**
+ * require the recommended rules from both ESLint React plugins
+ *
+ * https://github.com/yannickcr/eslint-plugin-react/blob/master/index.js
+ * https://github.com/facebook/react/blob/master/packages/eslint-plugin-react-hooks/index.js
+ */
+const reactRules = require('eslint-plugin-react').configs.recommended.rules
+const reactHooksRules = require('eslint-plugin-react-hooks').configs.recommended
+  .rules
 
-// make modifications to plugin's recommended rules
-rules['react/no-unsafe'] = 'warn' // all unsafe methods should be accompanied by an explainer comment
+// make modifications to eslint-plugin-react's recommended rules
+reactRules['react/no-unsafe'] = 'warn' // all unsafe methods should be accompanied by an explainer comment
 
 module.exports = {
   env: {
@@ -11,12 +17,9 @@ module.exports = {
   },
   plugins: ['react', 'react-hooks'],
   rules: {
-    // spread the recommended rules (includes above modifications)
-    ...rules,
-    // recommended rules from eslint-plugin-react-hooks
-    // https://github.com/facebook/react/tree/master/packages/eslint-plugin-react-hooks/
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    // spread both sets of recommended rules (includes above modifications)
+    ...reactRules,
+    ...reactHooksRules,
   },
   settings: {
     react: {
