2df0d086b371bd7af3ca6cb60ca007866b03b26b
Dustin Ruetz <dtj.ruetz@gmail.com>
2022-06-16 14:51:17 -0400

    docs(config/prettier): comments on Prettier code formatting in VS Code

--- src/config/.prettierrc.js
+++ src/config/.prettierrc.js
@@ -1,10 +1,10 @@
 /**
- * steps to enable code formatting on save of a file in Visual Studio Code (VS Code):
+ * To enable code formatting on save of a file in VS Code:
  *
- * 1. Press Cmd+, to open Settings
- * 2. Type "formatter" into the Settings searchbar
- * 3. Set the "Editor: Default Formatter" selector to "Prettier - Code formatter"
- * 4. Set the "Editor: Format On Save" checkbox to checked
+ * 1. Press Cmd+, to open Settings.
+ * 2. Type "formatter" into the Settings searchbar.
+ * 3. Set the "Editor: Default Formatter" selector to "Prettier - Code formatter".
+ * 4. Set the "Editor: Format On Save" checkbox to checked.
  */
 module.exports = {
   // https://prettier.io/docs/en/options.html
